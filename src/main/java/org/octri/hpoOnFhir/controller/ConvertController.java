e770106c033f20a540c5149daf5bb4a29112dfe9
Amy Yates <yateam@ohsu.edu>
2018-06-18 15:27:46 -0700

    Use new return objects.

--- src/main/java/org/octri/hpoOnFhir/controller/ConvertController.java
+++ src/main/java/org/octri/hpoOnFhir/controller/ConvertController.java
@@ -1,30 +1,39 @@
-package org.octri.hpoOnFhir.controller;
+package org.octri.hpoonfhir.controller;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.hl7.fhir.dstu3.model.Observation;
-import org.monarchinitiative.loinc2hpo.fhir.FhirResourceParserDstu3;
-import org.monarchinitiative.phenol.ontology.data.Term;
-import org.octri.hpoOnFhir.service.ObservationAnalysisService;
+import org.monarchinitiative.fhir2hpo.hpo.HpoTermWithNegation;
+import org.octri.hpoonfhir.service.ObservationAnalysisService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.parser.IParser;
 
 @RestController
 public class ConvertController {
 	
 	@Autowired
 	ObservationAnalysisService observationAnalysisService;
+	private final IParser jsonParser;
+	
+	public ConvertController() {
+		jsonParser = FhirContext.forDstu3().newJsonParser();	
+	}
 	
 	@PostMapping("/convert")
 	public Map<String, Object> convert(String resource) throws Exception {
 		Map<String, Object> out = new HashMap<>();	
-		FhirResourceParserDstu3 parser = new FhirResourceParserDstu3();
-		Observation observation = (Observation) parser.parse(resource);
-		Term term = observationAnalysisService.analyzeObservationThreadsafe(observation);
-		out.put("hpoTerm", term == null ? "Not Found" : term.getName());
-		System.out.println(out.get("hpoTerm"));
+		
+		Observation observation = (Observation) jsonParser.parseResource(resource);
+		Set<HpoTermWithNegation> terms = observationAnalysisService.analyzeObservation(observation);
+		String result = terms.size() == 0 ? "Not Found": terms.stream().map(term -> term.toString()).collect(Collectors.joining(","));
+		out.put("hpoTerm", result);
 		return out;
 	}
 
