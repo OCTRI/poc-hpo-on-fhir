8cefad813dfc2dd8350ad818060c2eaf75397b86
Amy Yates <yateam@ohsu.edu>
2019-03-20 16:02:22 -0700

    Add display of request mappings to debug Epic.

--- src/main/java/org/octri/hpoonfhir/controller/MainController.java
+++ src/main/java/org/octri/hpoonfhir/controller/MainController.java
@@ -32,10 +32,14 @@ import org.octri.hpoonfhir.view.PatientModel;
 import org.octri.hpoonfhir.view.SummaryStatsModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 import ca.uhn.fhir.context.FhirContext;
 
@@ -59,7 +63,20 @@ public class MainController {
 	@Autowired
 	private HpoService hpoService;
 
-	/**
+	private final RequestMappingHandlerMapping handlerMapping;
+
+	 @Autowired
+	 public MainController(RequestMappingHandlerMapping handlerMapping) {
+		 this.handlerMapping = handlerMapping;
+	 }
+
+	 @GetMapping("/")
+	 public String show(Model model) {
+		 model.addAttribute("handlerMethods", this.handlerMapping.getHandlerMethods().keySet());
+		 return "wtf";
+	 } 
+	 
+	 /**
 	 * Return to a clean search form with no results.
 	 * 
 	 * @param model
