bd5978e1031f230efa5e803c94281e54c8da0ad1
Amy Yates <yateam@ohsu.edu>
2019-09-03 13:32:33 -0700

    Improvements for demonstration purposes
    - Clearly indicate that there is no PHI (https://github.com/OCTRI/poc-hpo-on-fhir/issues/30)
    - Replace search with prepopulated patients (https://github.com/OCTRI/poc-hpo-on-fhir/issues/31)

--- src/main/java/org/octri/hpoonfhir/controller/MainController.java
+++ src/main/java/org/octri/hpoonfhir/controller/MainController.java
@@ -1,5 +1,6 @@
 package org.octri.hpoonfhir.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -17,9 +18,7 @@ import org.octri.hpoonfhir.view.PatientModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -32,7 +31,7 @@ public class MainController {
 	FhirService fhirService;
 
 	/**
-	 * Return to a clean search form with no results.
+	 * Display a hardcoded list of patients that can be browsed
 	 * 
 	 * @param model
 	 * @param request
@@ -41,49 +40,20 @@ public class MainController {
 	@GetMapping("/")
 	public String home(Map<String, Object> model, HttpServletRequest request) {
 
-		// Load text describing caveats for the FHIR Service. This is
-		// temporary but will help users if we decide to deploy this to a public location.
-		String caveats = null;
-		if (fhirService.getServiceName().equals("SmartHealth IT")) {
-			caveats = "Note that many patients in this sandbox do not have Observations or the Observations do not "
-				+ "provide enough information to infer a phenotype. For a good demonstration, try Frank Taylor "
-				+ "or Kristyn Walker.";
-		} else if (fhirService.getServiceName().equals("Open Epic")) {
-			caveats = "Note that Open Epic only has a half dozen patients, and the API enforces both a first and "
-				+ "last name be used to search. Try Jason Argonaut, then select the id beginning with Tbt3KuC...";
+		List<PatientModel> patientModels = new ArrayList<>();
+		
+		try {
+			patientModels.add(new PatientModel(fhirService.findPatientById("smart-1627321")));
+			patientModels.add(new PatientModel(fhirService.findPatientById("smart-1134281")));
+			patientModels.add(new PatientModel(fhirService.findPatientById("smart-8888803")));
+		} catch (FHIRException e) {
+			// Do nothing. Assume something's wrong with the sandbox and display a message indicating this.
+			e.printStackTrace();
 		}
 		
 		model.put("fhirServiceName", fhirService.getServiceName());
-		model.put("caveats", caveats);
-		model.put("patientSearchForm", new PatientModel());
-		model.put("results", false);
-		return "search";
-	}
-
-	/**
-	 * Search using the form parameters, and return results.
-	 * 
-	 * @param model
-	 * @param form
-	 * @return
-	 */
-	@PostMapping("/")
-	public String search(Map<String, Object> model, @ModelAttribute PatientModel form) {
-		model.put("fhirServiceName", fhirService.getServiceName());
-		model.put("patientSearchForm", form);
-		try {
-			List<Patient> patients = fhirService.findPatientsByFullName(form.getFirstName(), form.getLastName());
-			List<PatientModel> patientModels = patients.stream()
-					.map(fhirPatient -> new PatientModel(fhirPatient))
-					.collect(Collectors.toList());
-			model.put("patients", patientModels);
-			model.put("results", true);
-		} catch (FHIRException e) {
-			// TODO: Decide how to handle FHIRException which is only thrown if there's an error in the conversion of V2
-			// messages
-			e.printStackTrace();
-		}
-		return "search";
+		model.put("patients", patientModels);
+		return "browse";
 	}
 
 	/**
